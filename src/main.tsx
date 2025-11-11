e16a11ffdc6c9cd05be1ccd752aae991517c6410
Danish <shazaibdanish4@gmail.com>
2025-11-11 16:41:12 +0500

    files updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/my-portfolio/">
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>
 );
