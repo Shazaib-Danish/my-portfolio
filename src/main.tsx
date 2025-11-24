3ec58a9702363084f49d7d3bfd3b92aaf5921a8d
Danish <shazaibdanish4@gmail.com>
2025-11-25 04:04:12 +0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HelmetProvider>
+    <HashRouter>
       <App />
-    </HelmetProvider>
+    </HashRouter>
   </StrictMode>
 );
